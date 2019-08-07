06efdf3481c3db01fb0f4789d34afb532ea56b74
Kritsatorn <kritsatorn@billme.co.th>
2019-08-07 14:39:15 +0700

    without  abacuz

--- src/App.js
+++ src/App.js
@@ -5,7 +5,21 @@ function App() {
   return (
     <div className="App">
       <div className="Body">
+
+      <div className="abacuz">
         ABACUZ
+      </div>
+      <div className="box">
+          <div className="sub-text">
+          what we do?
+          </div>
+            <div className="qoute">
+            We grow
+          </div>
+          <div className="qoute" style={ {color: '#F61067' }}>
+            great developer
+          </div>
+      </div>
       </div>
     </div>
   );
